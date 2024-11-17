0b7e548a6381d21552890b9761a6038557fd2851
Axel Rodrigo Sotelo Ramírez <95106582+arodrigosote@users.noreply.github.com>
2024-11-16 19:49:06 -0600

    Update nodes.py

--- controllers/nodes.py
+++ controllers/nodes.py
@@ -1,4 +1,4 @@
-import os
+from scapy.all import ARP, Ether, srp
 import socket
 
 def get_network_nodes():
@@ -6,24 +6,27 @@ def get_network_nodes():
     hostname = socket.gethostname()
     local_ip = socket.gethostbyname(hostname)
 
-    if local_ip.startswith("192."):
-        # Obtener la dirección IP de la interfaz de red activa
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        try:
-            # No se envía ningún dato, solo se utiliza para obtener la IP
-            s.connect(("8.8.8.8", 80))
-            local_ip = s.getsockname()[0]
-        except Exception as e:
-            raise RuntimeError("No se pudo obtener la dirección IP de la interfaz de red") from e
-        finally:
-            s.close()
+    # Obtener la dirección IP de la interfaz de red activa
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # No se envía ningún dato, solo se utiliza para obtener la IP
+        s.connect(("8.8.8.8", 80))
+        local_ip = s.getsockname()[0]
+    except Exception as e:
+        raise RuntimeError("No se pudo obtener la dirección IP de la interfaz de red") from e
+    finally:
+        s.close()
 
-    subnet = '.'.join(local_ip.split('.')[:-1]) + '.'
+    # Calcular el rango de la subred
+    subnet = '.'.join(local_ip.split('.')[:-1]) + '.0/24'
 
-    for i in range(1, 255):
-        ip = subnet + str(i)
-        response = os.system(f"ping -c 1 -w 1 {ip} > /dev/null 2>&1")
-        if response == 0:
-            nodes.append({'id': i, 'ip': ip})
+    # Crear el paquete ARP para el escaneo de la red
+    paquete = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=subnet)
+
+    # Enviar el paquete y recibir respuestas
+    resultado = srp(paquete, timeout=2, verbose=False)[0]
+
+    for envio, respuesta in resultado:
+        nodes.append({'ip': respuesta.psrc, 'mac': respuesta.hwsrc})
 
     return nodes
