46aeb921a88dc5ad2d8d4a57bd135cffbf97a3ab
Axel Rodrigo Sotelo Ramírez <95106582+arodrigosote@users.noreply.github.com>
2024-11-17 00:37:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
+import socket
+import threading
 from controllers.nodes import get_network_nodes
 from utils.menu import mostrar_menu
-import threading
-import socket
 
 def handle_client(client_socket):
-    # Manejar la conexión con el cliente
-    client_socket.send(b"Conexion establecida con el servidor")
-    client_socket.close()
+    try:
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            response = f"Comando recibido: {data.decode()}"
+            client_socket.send(response.encode())
+    except Exception as e:
+        print(f"Error con el cliente: {e}")
+    finally:
+        client_socket.close()
 
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(("0.0.0.0", 9999))
-    server.listen(15)
-    print("Servidor escuchando en el puerto 9999")
-
-    while True:
-        client_socket, addr = server.accept()
-        print(f"Conexión aceptada de {addr}")
-        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
-        client_handler.start()
+    try:
+        server.bind(("0.0.0.0", 9999))
+        server.listen(15)
+        print("Servidor escuchando en el puerto 9999")
+        while True:
+            client_socket, addr = server.accept()
+            print(f"Conexión aceptada de {addr}")
+            client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+            client_handler.start()
+    except Exception as e:
+        print(f"Error al iniciar el servidor: {e}")
+    finally:
+        server.close()
 
 def connect_clients(nodes):
     for node in nodes:
@@ -37,40 +49,17 @@ def opcion1():
     print("Nodos en la red:")
     for node in nodes:
         print(f"ID: {node['id']}, IP: {node['ip']}, MAC: {node['mac']}")
-    
     connect_clients(nodes)
-    
-
-    
-
-def opcion2(nodes):
-    print("Opción 3 seleccionada")
-
-def opcion3():
-    print("Opción 3 seleccionada")
-
-def opcion4():
-    print("Opción 4 seleccionada")
-
-
 
 def main():
-    # Iniciar el servidor en un hilo separado
-    server_thread = threading.Thread(target=start_server)
+    server_thread = threading.Thread(target=start_server, daemon=True)
     server_thread.start()
-
 
     while True:
         mostrar_menu()
         opcion = input("Seleccione una opción: ")
         if opcion == '1':
             opcion1()
-        elif opcion == '2':
-            opcion2()
-        elif opcion == '3':
-            opcion3()
-        elif opcion == '4':
-            opcion4()
         elif opcion == '5':
             print("Saliendo...")
             break
@@ -79,5 +68,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
