06013d7246f0218b9df02d7481a94be8d35d38b2
Axel Rodrigo Sotelo Ramírez <95106582+arodrigosote@users.noreply.github.com>
2024-11-21 15:00:03 -0600

    Update emergency_room.py

--- models/emergency_room.py
+++ models/emergency_room.py
@@ -78,7 +78,7 @@ def activar_sala(ip, nodo_maestro):
                 log_message("[Nodo] El nodo propio no es el nodo maestro.")
                 codigo = "11"
                 mensaje = f"UPDATE salas_emergencia SET estado = 'activado' WHERE ip = '{ip}'"
-                enviar_mensaje_a_maestro(nodo_maestro[2], codigo, mensaje)
+                enviar_mensaje_a_maestro(nodo_maestro, codigo, mensaje)
         else:
             log_message("\n[Nodo Propio] No se encontró el nodo propio.")
     except sqlite3.Error as e:
