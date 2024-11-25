8feb8af5033ddd5c4f34c45ef505036891c1b926
Axel Rodrigo Sotelo Ramírez <95106582+arodrigosote@users.noreply.github.com>
2024-11-24 18:41:52 -0600

    update

--- controllers/server_client.py
+++ controllers/server_client.py
@@ -174,8 +174,8 @@ def connect_to_node(node):
     # Generar un ID basado en la IP
     print(node)
     print(node[0])
-    node_id = int(node[0].get("id"))
-    node_ip = int(node[0].get("ip"))
+    node_id = int(node[3])
+    node_ip = node[0]
     if node_id in [1, 2, 254]:  # Opcional: omitir nodos específicos
         return None
 
