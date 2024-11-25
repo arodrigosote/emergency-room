30c512fccbfdb7a7f4ad8ba050967a487fc1e793
Axel Rodrigo Sotelo Ramírez <95106582+arodrigosote@users.noreply.github.com>
2024-11-24 19:53:47 -0600

    Update database.py

--- controllers/database.py
+++ controllers/database.py
@@ -122,7 +122,7 @@ def ejecutar_dbchanges():
         linea = linea.strip()
         if linea:
             try:
-                fecha_hora, consulta = linea.split("#", 1)
+                fecha_hora, consulta = linea.split("- #", 1)
                 registros.append((datetime.strptime(fecha_hora, "%Y-%m-%d %H:%M:%S"), consulta))
                 
             except ValueError:
