d6f30a743c300b5d3629610a4f97145b0abe1d5d
Axel Rodrigo Sotelo Ramírez <95106582+arodrigosote@users.noreply.github.com>
2024-11-26 12:21:46 -0600

    quitando paralelismo de nodo

--- models/node.py
+++ models/node.py
@@ -4,6 +4,7 @@ from utils.log import log_message
 from controllers.server_client import get_client_socket_by_ip, active_connections, unactive_connections, elegir_nodo_maestro  # Asegúrate de tener una función para obtener el socket del cliente
 from controllers.nodes import get_network_nodes, get_own_node
 from models.database import guardar_cambios_db_changestomake
+from controllers.handle_down import verificar_conexiones
 
 
 def obtener_nodo_propio(cursor, own_node_ip):
@@ -65,6 +66,7 @@ def enviar_mensajes_a_todos(codigo, mensaje, incluir_propio=False):
 
 
 def procesar_consulta(consulta, es_compleja=False):
+    verificar_conexiones()
     """Procesa consultas simples o complejas dependiendo del tipo."""
     try:
         master_node_id = max(active_connections.keys())
@@ -120,174 +122,3 @@ def solicitar_cambios_db():
     except Exception as e:
         log_message(f"[Error] {str(e)}")
 
-
-# def enviar_consulta_sencilla(consulta):
-#     try:
-#         master_node_id = max(active_connections.keys())
-#         master_node_ip = active_connections[master_node_id]
-#         own_node = get_own_node()
-
-#         with sqlite3.connect('nodos.db') as conn:
-#             cursor = conn.cursor()
-#             nodo_propio = obtener_nodo_propio(cursor, own_node['ip'])
-
-#             if nodo_propio:
-#                 if nodo_propio[2] == master_node_ip:
-#                     log_message("[Nodo] El nodo propio es el nodo maestro.")
-#                     enviar_mensaje_a_todos("10", consulta)
-#                 else:
-#                     log_message("[Nodo] El nodo propio no es el nodo maestro.")
-#                     enviar_mensaje_a_maestro(master_node_ip, "11", consulta)
-#             else:
-#                 log_message("\n[Nodo Propio] No se encontró el nodo propio.")
-#     except Exception as e:
-#         log_message(f"[Error] {str(e)}")
-
-# def enviar_consulta_compleja(consulta):
-#     try:
-#         master_node_id = max(active_connections.keys())
-#         master_node_ip = active_connections[master_node_id].getpeername()[0]
-#         master = active_connections[master_node_id]
-#         own_node = get_own_node()
-
-#         with sqlite3.connect('nodos.db') as conn:
-#             cursor = conn.cursor()
-#             nodo_propio = obtener_nodo_propio(cursor, own_node['ip'])
-
-#             if nodo_propio:
-#                 if nodo_propio[2] == master_node_ip:
-#                     sala, cama = obtener_sala_y_cama()
-#                     if sala and cama:
-#                         consulta = consulta.replace("00", str(sala)).replace("01", str(cama))
-#                         log_message("[Nodo] El nodo propio es el nodo maestro.")
-#                         enviar_mensaje_a_todos_incluyendo_propio("10", consulta)
-#                         log_message(f"[Nodo] Se asignó la cama {cama} en la sala {sala} al paciente.")
-#                     else:
-#                         log_message("[Nodo] No hay camas disponibles en ninguna sala.")
-#                         print("No hay camas disponibles en ninguna sala.")
-#                 else:
-#                     log_message("[Nodo] El nodo propio no es el nodo maestro.")
-#                     enviar_mensaje_a_maestro_calculo_sala(master, "11", consulta)
-#             else:
-#                 log_message("\n[Nodo Propio] No se encontró el nodo propio.")
-#     except Exception as e:
-#         log_message(f"[Error] {str(e)}")
-
-
-
-# def enviar_mensaje_a_todos(codigo_instruccion, mensaje):
-#     hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#     mensaje_completo = f"{codigo_instruccion}|{hora_actual}|{mensaje}"
-#     respuestas = []
-#     own_node = get_own_node()
-    
-#     for destino, client_socket in active_connections.items():
-#         try:
-#             # Obtener la IP del nodo destino
-#             destino_ip = client_socket.getpeername()[0]
-            
-#             # Verificar que el nodo destino no sea el nodo propio
-#             if destino_ip != own_node['ip']:
-#                 if client_socket.fileno() != -1:  # Verifica que el socket siga activo
-#                     client_socket.send(mensaje_completo.encode())
-#                     log_message(f"[Mensaje enviado] A nodo {destino}: {mensaje_completo}")
-                    
-#                     # Analizar la respuesta del servidor
-#                     respuesta = client_socket.recv(1024).decode()  # Tamaño del buffer ajustable
-#                     log_message(f"[Respuesta recibida] De nodo {destino}: {respuesta}")
-#                     respuestas.append(respuesta)
-#                 else:
-#                     log_message(f"[Error] La conexión con el nodo {destino} no está activa.")
-#             else:
-#                 log_message(f"[Nodo Propio] No se envía mensaje al nodo propio.")
-#         except Exception as e:
-#             log_message(f"[Error] No se pudo enviar el mensaje a nodo {destino}: {e}")
-    
-#     # Verificar si todas las respuestas son "OK"
-#     if all(respuesta == "OK" for respuesta in respuestas):
-#         log_message("[Consenso] Todos los nodos respondieron OK")
-#     else:
-#         log_message("[Sin consenso] No todos los nodos respondieron OK")
-
-# def enviar_mensaje_a_todos_incluyendo_propio(codigo_instruccion, mensaje):
-#     hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#     mensaje_completo = f"{codigo_instruccion}|{hora_actual}|{mensaje}"
-#     respuestas = []
-#     own_node = get_own_node()
-    
-#     for destino, client_socket in active_connections.items():
-#         try:            
-#             # Verificar que el nodo destino no sea el nodo propio
-#             if client_socket.fileno() != -1:  # Verifica que el socket siga activo
-#                 client_socket.send(mensaje_completo.encode())
-#                 log_message(f"[Mensaje enviado] A nodo {destino}: {mensaje_completo}")
-                    
-#                 # Analizar la respuesta del servidor
-#                 respuesta = client_socket.recv(1024).decode()  # Tamaño del buffer ajustable
-#                 log_message(f"[Respuesta recibida] De nodo {destino}: {respuesta}")
-#                 respuestas.append(respuesta)
-#             else:
-#                 log_message(f"[Nodo Propio] No se envía mensaje al nodo propio.")
-#         except Exception as e:
-#             log_message(f"[Error] No se pudo enviar el mensaje a nodo {destino}: {e}")
-    
-#     # Verificar si todas las respuestas son "OK"
-#     if all(respuesta == "OK" for respuesta in respuestas):
-#         log_message("[Consenso] Todos los nodos respondieron OK")
-#     else:
-#         log_message("[Sin consenso] No todos los nodos respondieron OK")
-
-        
-# def enviar_mensaje_a_maestro(client, codigo, mensaje):
-#     hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#     mensaje_completo = f"{codigo}|{hora_actual}|{mensaje}"
-#     #nodo_maestro = get_client_socket_by_ip(ip_nodo_maestro)  # Obtener el socket del nodo maestro usando su IP
-#     try:
-#         if client.fileno() != -1:  # Verifica que el socket siga activo
-#             client.send(mensaje_completo.encode())
-#             log_message(f"[Mensaje enviado] A nodo maestro: {mensaje_completo}")
-            
-#             # Analizar la respuesta del servidor
-#             respuesta = client.recv(1024).decode()  # Tamaño del buffer ajustable
-#             log_message(f"[Respuesta recibida] De nodo maestro: {respuesta}")
-            
-#             if respuesta == "OK":
-#                 log_message("[Consenso] El nodo maestro respondió OK, Query ejecutada en todos nodos")
-#             else:
-#                 log_message("[Sin consenso] El nodo maestro no respondió OK, Error")
-                
-#         else:
-#             log_message("[Error] La conexión con el nodo maestro no está activa.")
-#     except Exception as e:
-#         print(f"[Error] No se pudo enviar el mensaje al nodo maestro: {e}")
-#         print(f"[Debug] Detalles del error: {str(e)}")
-#         import traceback
-
-# def enviar_mensaje_a_maestro_calculo_sala(master, codigo, mensaje):
-#     hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#     sala, cama = obtener_sala_y_cama()
-#     if sala and cama:
-#         # Reemplazar "00" con el valor de sala y "01" con el valor de cama
-#         mensaje = mensaje.replace("00", str(sala)).replace("01", str(cama))
-#     else:
-#         log_message("[Nodo] No hay camas disponibles en ninguna sala.")
-#         print("No hay camas disponibles en ninguna sala.")
-#     mensaje_completo = f"{codigo}|{hora_actual}|{mensaje}"
-#     try:
-#         if master.fileno() != -1:  # Verifica que el socket siga activo
-#             master.send(mensaje_completo.encode())
-#             log_message(f"[Mensaje enviado] A nodo maestro: {mensaje_completo}")
-            
-#             # Analizar la respuesta del servidor
-#             respuesta = master.recv(1024).decode()  # Tamaño del buffer ajustable
-#             log_message(f"[Respuesta recibida] De nodo maestro: {respuesta}")
-            
-#             if respuesta == "OK":
-#                 log_message("[Consenso] El nodo maestro respondió OK, Query ejecutada en todos nodos")
-#             else:
-#                 log_message("[Sin consenso] El nodo maestro no respondió OK, Error")
-                
-#         else:
-#             log_message(f"[Error] La conexión con el nodo maestro no está activa.")
-#     except Exception as e:
-#         log_message(f"[Error] No se pudo enviar el mensaje al nodo maestro: {e}")
